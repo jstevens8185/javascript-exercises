f4326f37e9e1a51c29a2016ed751426d84582c1a
jstevens8185 <j.stevens8185@gmail.com>
2022-06-09 12:21:19 -0500

    Complete function 'sumAll'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0){
+        return 'ERROR';
+    }
+    if(typeof(a) != 'number' || typeof(b) != 'number'){
+        return 'ERROR';
+    }
+    let result = 0;
+    if(a < b){
+        result = a;
+        for (; a < b; a++){
+            //console.log(`${result} += ${a + 1}: `);
+            result += (a + 1);
+            //console.log(result + "\n");
+        }
+        //console.log(`\n\nfinal result is: ${result}`)
+        return result;
+    }
+    if(b < a){
+        result = b;
+        for(; b <a; b++){
+            result += (b + 1);
+        }
+        return result;
+    }
 
 };
 
