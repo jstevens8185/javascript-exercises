6005e88e32fe94dd7975e55007046ab30ad40656
jstevens8185 <j.stevens8185@gmail.com>
2022-06-09 10:10:49 -0500

    Complete code in 'removeFromArray.js'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argugments) {
+    let newArray = array;
+    //console.log(argugments.length);
+    for(let i = 0; i < argugments.length; i++){
+        //console.log('in for loop\n');
+        if (newArray.indexOf(argugments[i]) != -1){
+            newArray.splice((newArray.indexOf(argugments[i])), 1);
+        }
+    }
+        //console.log(newArray.indexOf(argugments));
+    return newArray; 
 };
 
 // Do not edit below this line
